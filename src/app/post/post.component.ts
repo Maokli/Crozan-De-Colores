a6b0f679d1c600777dd7c2172f6bfeb4ee15659c
Maokli2X <louayinfo07@gmail.com>
2021-01-10 21:39:58 +0100

    Added Comments with Disqus

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -31,6 +31,7 @@ export class PostComponent implements OnInit {
       }
     });
     this.GetAllCategories();
+    this.EnableDisqus();
   }
   ngAfterViewInit(){
     this.GetPost();
@@ -109,5 +110,17 @@ export class PostComponent implements OnInit {
         this.categories.push(story.content);
       });
     });
+  }
+  EnableDisqus() {
+    var disqus_config = function () {
+      this.page.url = window.location.hostname;  // Replace PAGE_URL with your page's canonical URL variable
+      this.page.identifier = window.location.pathname; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+      };
+      (function() { // DON'T EDIT BELOW THIS LINE
+      var d = document, s = d.createElement('script');
+      s.src = 'https://corazondecolores.disqus.com/embed.js';
+      s.setAttribute('data-timestamp', new Date().toString());
+      (d.head || d.body).appendChild(s);
+      })();
   }
 }
